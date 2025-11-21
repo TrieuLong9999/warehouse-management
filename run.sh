d69858368ac5998e726d1b016b8cf979ecf0024e
TrieuLong9999 <151723388+TrieuLong9999@users.noreply.github.com>
2025-11-22 06:50:34 +0700

    init file run

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@ docker stop warehouse-management || true
 docker rm -f warehouse-management || true    
 
 ## pull github repo
-git checkout main
-git pull origin main
+git checkout new-version
+git pull origin new-version
 
 ## build docker image
 docker build -t warehouse-management .
